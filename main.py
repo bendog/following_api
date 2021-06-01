1c10f07d01b2ff4143906d1c95199bef3f4fe3ca
Ben Fitzhardinge <ben@fitzhardinge.net>
2021-06-02 00:11:31 +0800

    updated to use UUID and added FollowingStatus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3.9
 import os
-from typing import List
+from typing import Dict, List, Optional
 
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
@@ -14,9 +14,12 @@ WEBSOCKET_HOST = os.environ.get("WEBSOCKET_HOST", "ws://localhost:8000")
 class ConnectionManager:
     def __init__(self):
         self.active_connections: List[WebSocket] = []
+        self.history: List[str] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
+        for message in self.history:
+            await websocket.send_text(message)
         self.active_connections.append(websocket)
 
     def disconnect(self, websocket: WebSocket):
@@ -27,10 +30,42 @@ class ConnectionManager:
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
+            self.history.append(message)
             await connection.send_text(message)
 
 
+class FollowingStatus:
+    def __init__(self):
+        self._status: Dict[str, Optional[int]] = {}
+
+    async def add_client(self, client_id: str):
+        """ add a new client to the system """
+        self._status[client_id] = 0
+
+    async def remove_client(self, client_id: str):
+        """ remove the client from the system """
+        del self._status[client_id]
+
+    async def update_value(self, client_id: str, value: int):
+        """ update the following value for the user """
+        self._status[client_id] += value
+
+    def client_status(self, client_id: str) -> int:
+        """ return the current users score """
+        return self._status[client_id]
+
+    @property
+    def average(self) -> float:
+        """ return the average of following users """
+        return sum(self._status.values()) / len(self._status)
+
+    def __dict__(self) -> dict:
+        return self._status
+
+
+
 manager = ConnectionManager()
+FOLLOWING_STATUS = FollowingStatus()
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -44,13 +79,20 @@ async def index(request: Request):
 
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
     await manager.connect(websocket)
+    await FOLLOWING_STATUS.add_client(client_id) 
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            try:
+                await FOLLOWING_STATUS.update_value(client_id, int(data))
+                await manager.broadcast(f"Client #{client_id} adjusted their score to: {FOLLOWING_STATUS.client_status(client_id)}")
+                await manager.broadcast(f"Following Value is {FOLLOWING_STATUS.average:.1f}")
+            except ValueError:
+                await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{client_id} left the chat")
+        await FOLLOWING_STATUS.remove_client(client_id)
