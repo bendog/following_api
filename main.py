9218ff522ff030be69d950ee8696cb9014effd0f
Ben Fitzhardinge <ben@fitzhardinge.net>
2021-06-01 23:11:51 +0800

    updated to use environment for hostname

--- main.py
+++ main.py
@@ -1,47 +1,14 @@
 #! /usr/bin/env python3.9
+import os
 from typing import List
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        <h2>Your ID: <span id="ws-id"></span></h2>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var client_id = Date.now()
-            document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+templates = Jinja2Templates("templates")
+WEBSOCKET_HOST = os.environ.get("WEBSOCKET_HOST", "ws://localhost:8000")
 
 
 class ConnectionManager:
@@ -66,9 +33,14 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    template = "index.html"
+    context = {
+        "request": request,
+        "websocket_host": WEBSOCKET_HOST
+    }
+    return templates.TemplateResponse(template, context)
 
 
 @app.websocket("/ws/{client_id}")
